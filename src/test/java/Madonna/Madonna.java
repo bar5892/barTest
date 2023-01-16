65cb7e01f2ddf62a129d48ce48da5fae89386b25
Aviem <Aviem@10.0.0.8>
2023-01-16 23:01:00 +0200

    new_update

--- src/test/java/Madonna/Madonna.java
+++ src/test/java/Madonna/Madonna.java
@@ -1,9 +1,9 @@
 package Madonna;
 
+import static org.junit.Assert.*;
 import java.time.Duration;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
-
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -23,6 +23,7 @@ public class Madonna {
 	
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
+		//Open Browser
 		WebDriverManager.chromedriver().setup();
 		poModel.driver=new ChromeDriver();
 		poModel.driver.manage().window().maximize();
@@ -32,49 +33,43 @@ public class Madonna {
 
 	@AfterClass
 	public static void tearDownAfterClass() throws Exception {
+		//Close Browser and exit
 		poModel.driver.quit();
 	}
 
 	@Test
 	public void test1() throws Exception {
 		//test1
-		try {
-			PageFactory.initElements(poModel.driver, poModel.class);
-			poModel.serachInPutBox.sendKeys("madonna");
-			poModel.serachButton.click();
-			List<WebElement> list;
-			boolean condition=true; 
-			while(condition) {
-				list = poModel.driver.findElements(By.xpath("//h3//a[@id='video-title']"));
-		        //System.out.println("size list is : "+list.size());
-		        for(int i = 0; i < list.size(); i++) {
-		        	if(list.get(i).getText().contains("Justify My Love")) {
-		        		//System.out.println(list.get(i).getText());
-		                list.get(i).click();
-		                condition=false;
-		                break;
-		            }
+		//Madonna search
+		PageFactory.initElements(poModel.driver, poModel.class);
+		poModel.serachInPutBox.sendKeys("madonna");
+		poModel.serachButton.click();
+		List<WebElement> list;
+		boolean condition=true; 
+		// searching for the right song loop
+		while(condition) {
+			list = poModel.driver.findElements(By.xpath("//h3//a[@id='video-title']"));
+		    for(int i = 0; i < list.size(); i++) {
+		    	if(list.get(i).getText().contains("Justify My Love")) {
+		    		list.get(i).click();
+		            condition=false;
+		            break;
 		        }
-		        js=(JavascriptExecutor)poModel.driver;
-		        js.executeScript("window,scrollBy(0,5000)","");
-		        //poModel.driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(50));
-		        Thread.sleep(5000);
-		        //poModel.driver.manage().timeouts().implicitlyWait(100,TimeUnit.SECONDS);
-		    }	
-			try {
-				advertisement=poModel.driver.findElement(By.xpath("//DIV//SPAN[@CLASS='ytp-ad-simple-ad-badge']")); 
-				if(advertisement.getAttribute("id").contains("simple-ad-badge:")) {
-					System.out.println("There is an advertisement at the beginning of the clip");
-				}
-		    }
-		    catch(Exception e) {
-				System.out.println("There isn't an advertisement at the beginning of the clip");	
-		    }
+		   }
+		   // scrolling the result screen
+		   js=(JavascriptExecutor)poModel.driver;
+		   js.executeScript("window,scrollBy(0,5000)","");
+		   Thread.sleep(2000);
+		}	
+		try {
+			// advertisement check
+			advertisement=poModel.driver.findElement(By.xpath("//DIV//SPAN[@CLASS='ytp-ad-simple-ad-badge']")); 
+			assertTrue(advertisement.getAttribute("id").contains("ad"));
+			System.out.println("There is an advertisement at the beginning of the clip");
 		}
 		catch(Exception e) {
-			System.out.println("Error test :" +e);
-		}
+			System.out.println("There isn't an advertisement at the beginning of the clip");	
+		}	
 	}
 }
-
 
--- src/test/java/Madonna/poModel.java
+++ src/test/java/Madonna/poModel.java
@@ -1,27 +1,13 @@
 package Madonna;
 
-import java.util.List;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class poModel {
-	
 	static WebDriver driver;
-	
-//	@FindBy(id="search")
-//	static WebElement serachInPutBox;
-	//@FindBy(xpath="//div[@id='sb_ifc50']//input[@id='search']")
-	//static List <WebElement> serachResults;
-//	@FindBy(id="quantity")
-//	static WebElement quantity;
 	@FindBy(xpath="//input[@id='search']")
 	static WebElement serachInPutBox;
 	@FindBy(xpath="//button[@id='search-icon-legacy']")
 	static WebElement serachButton;
-//	@FindBy(xpath="//DIV//SPAN[@CLASS='ytp-ad-simple-ad-badge']")
-//	static WebElement advertisement;
-	
-
 }
